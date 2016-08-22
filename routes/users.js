e0c665ac62e423372bf533adb9941d51de1f378a
MD. JAHIR ALAM <johiralamraju@gmail.com>
2016-08-22 02:30:07 +0000

    s

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+router.get('/create', function (req, res, next) {
+    var vm = {
+        title: "create user"
+    }
+    res.render('users/create', vm);
 });
 
 module.exports = router;
